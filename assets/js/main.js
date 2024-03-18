0373426fe4f6f8ca5279af97cf8ebafea3063991
saulsneider7 <saulsneider@hotmail.com>
2024-03-18 13:56:32 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+console.log('%c¡Atención! %c', 'font-size: 24px; color: yellow; background-color: red;', 'font-size: 20px;');
+console.log('%cEsta es una zona de desarrollo. No puedes ingresar a este apartado del sitio web.', 'font-size: 20px;');
+
+// EMAIL JS
+
+const btn = document.getElementById('cotizar');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.innerHTML = `<i class="fa-solid fa-paper-plane"></i>&nbsp;&nbsp; Cotizando ...`;
+
+        const serviceID = 'default_service';
+        const templateID = 'template_tb5qpw7';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.innerHTML = `<i class="fa-solid fa-paper-plane"></i>&nbsp;&nbsp; Soluciones Informaticas`;
+                let presupuesto = parseFloat(document.getElementById('presupuesto').value);
+                if (presupuesto > 299) {
+                    Swal.fire({
+                        title: "Formulario Enviado",
+                        text: "Nos comunicaremos en breve para enviarle una cotización.",
+                        icon: "success",
+                    }).then(() => {
+                        location.reload();
+                    })
+                } else {
+                    location.href = 'gracias.html';
+                }
+            }, (err) => {
+                alert(JSON.stringify(err));
+            });
+    });
+
+
+
+
+
